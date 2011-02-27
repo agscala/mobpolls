558b9c4fcd227402b774d02d925199027f5d271e
Andrew Scala <agscala@gmail.com>
2011-02-26 20:47:46 -0500

    Init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mobpolls::Application.config.secret_token = '19cde780b5081bd238232457cabee148a09b655270f445707e8db6cbb923016e7e5c5ecf16943deb09263b1d2a4ca79cc3f2f9073da8fd5b93e02f26c48feeed'
