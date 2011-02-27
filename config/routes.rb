707b317371202d96122b966975d5b423cdf6433f
Andrew Scala <agscala@gmail.com>
2011-02-26 20:56:25 -0500

    Adding user and question scaffolds

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Mobpolls::Application.routes.draw do
+  resources :users
+
+  resources :questions
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -48,7 +52,7 @@ Mobpolls::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => "welcome#index"
+  root :to => "question#index"
 
   # See how all your routes lay out with "rake routes"
 
